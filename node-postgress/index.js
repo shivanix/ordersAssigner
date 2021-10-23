91074ab0896595db050e94b6050d0506246ae3bb
Shivani <shvni.here@gmail.com>
2021-10-23 01:27:55 -0400

    Add cors

--- node-postgress/index.js
+++ node-postgress/index.js
@@ -2,6 +2,11 @@ const express = require("express")
 const app = express();
 PORT = 5000
 
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
 app.listen(PORT, () =>{
   console.log(`Server has started on port ${PORT}`);
 })
