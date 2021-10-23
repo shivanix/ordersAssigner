e99bbe6b7e35c25ec56ebf12be3f5c6fd71ea42e
Shivani <shvni.here@gmail.com>
2021-10-23 16:24:26 -0400

    Add route for get all drivers

--- node-postgress/index.js
+++ node-postgress/index.js
@@ -7,6 +7,19 @@ PORT = 5000
 app.use(cors());
 app.use(express.json())
 
+/***********************************Routes******************************************/
+
+/**Get all drivers from drivers entity from db */
+
+app.get("/", async (req, res) => {
+  // console.log("Backendddddd connected");
+  try {
+    const allDrivers = await pool.query("SELECT * FROM drivers");
+    res.json(allDrivers.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 
 app.listen(PORT, () =>{
