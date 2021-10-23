ac753f5bea5f5143473974a5e658652d1744cec5
Shivani <shvni.here@gmail.com>
2021-10-23 02:05:48 -0400

    Add seeds for orders entity

---
+++ db/seeds/orders.sql
@@ -0,0 +1,11 @@
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(2, 'Construction material', 100, 4200.00, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(2, 'Construction material', 71.38, 3948.45, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(2, 'Wood and Lumber', 263.88, 1950.52, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(2, 'Wood and Lumber', 116.98, 4991.45, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(3, 'Meat', 279.17, 6739.72, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(3, 'Meat', 537.91, 3618.08, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(3, 'Fresh Produce', 420.69, 5345.91, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(3, 'Farm Supplies', 171.13, 7429.78, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(3, 'Cheetos', 310.38, 7231.98, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(4, 'Rose Rocket Swag Shirts', 350.79, 5404.24, true);
+INSERT INTO orders(driver_id, description, cost, revenue_amount, assigned) VALUES(1, 'Rose Rocket Swag kits', 150.23, 6666.66, false);
