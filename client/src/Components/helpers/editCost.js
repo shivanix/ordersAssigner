1b4e2eb62e25541d9c11e0cb1f7954c6b668916a
Shivani <shvni.here@gmail.com>
2021-10-23 22:50:42 -0400

    Add conditional logic for unassigned orders

--- client/src/Components/helpers/editCost.js
+++ client/src/Components/helpers/editCost.js
@@ -1,18 +1,24 @@
 const EditCost = async (id, cost, driverId) => {
 
-  try {
-      const body = { cost };
+    if(driverId !== 1){
+        console.log("Not editable");
+        return;
+    }
 
-      const response = await fetch(`http://localhost:5000/cost/${id}`, {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body),
-      });
 
-      console.log("Editinf costtt")
-  } catch (error) {
-      console.log(error);
-  }
+    try {
+        const body = { cost };
+
+        const response = await fetch(`http://localhost:5000/cost/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+        });
+
+        console.log("Editinf costtt")
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export default EditCost;
