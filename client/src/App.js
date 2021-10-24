dd96ea01e551e2994dd2f3067efb48af68875f06
Shivani <shvni.here@gmail.com>
2021-10-23 22:15:58 -0400

    Add func to fetch data for orders from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
-import { useState } from 'react';
-import './App.css';
-import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
-
-
+import { useState } from "react";
+import "./App.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 // const itemsFromBackend=[
 //   {id:'11aa', content: 'First order'},
 //   {id:'22aa', content: 'Second order'}
 // ]
 
-// const columnsFromBackend = 
+// const columnsFromBackend =
 //   {
 //     ['Hello1']:{
 //       name: 'StackOrders',
@@ -17,62 +15,78 @@ import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 //     }
 //   }
 
-
-
 function App() {
   const [columns, setColumns] = useState(columnsFromBackend);
 
-    // Func to fetch data for drivers from db
-    const getDriver = async () => {
-      try {
-          const response = await fetch("http://localhost:5000");
-          const jsonData = await response.json();
+  // Func to fetch data for drivers from db
+  const getDriver = async () => {
+    try {
+      const response = await fetch("http://localhost:5000");
+      const jsonData = await response.json();
 
-          setDrivers(jsonData);
-      } catch (error) {
-          console.log(error.message);
-      }
+      setDrivers(jsonData);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
+  // Func to fetch data for orders from db
+  const getOrder = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/orders");
+      const jsonData = await response.json();
+
+      setOrders(jsonData);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="App">
- <DragDropContext onDragEnd = {result =>{console.log(result)}}>
- {Object.entries(columns).map((id, column) =>{
-   return(
-    <Droppable droppableId={id} key={id}>
-    {(provided, snapshot) => {
-        return (
-            <div
-                className="column-container"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                style={{
-                    background: snapshot.isDraggingOver
+      <DragDropContext
+        onDragEnd={(result) => {
+          console.log(result);
+        }}
+      >
+        {Object.entries(columns).map((id, column) => {
+          return (
+            <Droppable droppableId={id} key={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    className="column-container"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
                         ? "lightblue"
                         : "lightgrey",
-                    padding: 4,
-                    width: 250,
-                    minHeight: 500,
-                    border: "5px solid black",
-                }}
-            >
-                {/* {console.log("columnnnnnnn", column.items)} */}
-                {column.items.map((item, index) => {
-                    //  console.log('ITEM:', item);
-                    return (
-                     <Draggable key={item.id} draggableId={item.id} index={index}>
-                         </Draggable>
-                    )
-                })}
-                {provided.placeholder}
-            </div>
-        );
-    }}
-</Droppable>
-   )
- })}
-   </DragDropContext>
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500,
+                      border: "5px solid black",
+                    }}
+                  >
+                    {/* {console.log("columnnnnnnn", column.items)} */}
+                    {column.items.map((item, index) => {
+                      //  console.log('ITEM:', item);
+                      return (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
+                        ></Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
