b49c32395d33b5484e7bea29b6351e33c363cf57
Shivani <shvni.here@gmail.com>
2021-10-23 22:03:20 -0400

    Add static data; Add Droppable component to wrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 
+
+
+const itemsFromBackend=[
+  {id:'11aa', content: 'First order'},
+  {id:'22aa', content: 'Second order'}
+]
+
+const columnsFromBackend = [
+  {
+    ['Hello1']:{
+      name: 'StackOrders',
+      items: [itemsFromBackend]
+    }
+  }
+]
+
+
 function App() {
+  const [columns, setColumns] = useState({});
+
   return (
     <div className="App">
  <DragDropContext onDragEnd = {result =>{console.log(result)}}>
-Add DragDropContext
+ {Object.entries(columns).map(([id, column]) =>{
+   return(
+    <Droppable droppableId={id} key={id}>
+    {(provided, snapshot) => {
+        return (
+            <div
+                className="column-container"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={{
+                    background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "lightgrey",
+                    padding: 4,
+                    width: 250,
+                    minHeight: 500,
+                    border: "5px solid black",
+                }}
+            >
+                {/* {console.log("columnnnnnnn", column.items)} */}
+                {column.items.map((item, index) => {
+                    //  console.log('ITEM:', item);
+                    return (
+                       <div>
+                         order compionemt
+                       </div>
+                    )
+                })}
+                {provided.placeholder}
+            </div>
+        );
+    }}
+</Droppable>
+   )
+ })}
    </DragDropContext>
     </div>
   );
