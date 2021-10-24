b671fa3e4f169e7f59fb014984d9a89f077d49be
Shivani <shvni.here@gmail.com>
2021-10-23 22:40:34 -0400

    Add onDragEnd attribute of DragDropContext comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-
+import EditCost from "./Components/helpers/editCost";
+import EditRevenue from "./Components/helpers/editRevenue";
+import UpdateDriverOrder from "./Components/helpers/updateDriverOrder"
 // const itemsFromBackend=[
 //   {id:'11aa', content: 'First order'},
 //   {id:'22aa', content: 'Second order'}
@@ -19,8 +21,52 @@ function App() {
   const [columns, setColumns] = useState({});
   const [drivers, setDrivers] = useState([]);
   const [orders, setOrders] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
+  const onDragEnd = async (result) => {
+    console.log("EAR CIL", columns);
+    if (!result.destination) return;
 
+    const {source, destination} = result;
+    console.log("CIL source dest", source, destination);
+    if (source.droppableId !== destination.droppableId) {
+        console.log("COLUMNS: ", columns, "result: ", result);
+        const sourceColumn = columns[source.droppableId];
+        const destColumn = columns[destination.droppableId];
+        const sourceItems = [...sourceColumn.items];
+        const destItems = [...destColumn.items];
+        const [removed] = sourceItems.splice(source.index, 1);
+        destItems.splice(destination.index, 0, removed);
+        UpdateDriverOrder(result.draggableId, destination.droppableId).then(()=> {
+            setColumns({
+                ...columns,
+                [source.droppableId]: {
+                    ...sourceColumn,
+                    items: sourceItems,
+                },
+                [destination.droppableId]: {
+                    ...destColumn,
+                    items: destItems,
+                },
+            });
+
+            console.log("CIL", columns);
+             setLoaded(false);
+        })
+    } else {
+        const column = columns[source.droppableId];
+        const copiedItems = [...column.items];
+        const [removed] = copiedItems.splice(source.index, 1);
+        copiedItems.splice(destination.index, 0, removed);
+        setColumns({
+            ...columns,
+            [source.droppableId]: {
+                ...column,
+                items: copiedItems,
+            },
+        });
+    }
+};
 
 
 
